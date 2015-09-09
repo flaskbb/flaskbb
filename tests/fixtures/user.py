9d18cdd484ee7aa05b48c3eb9d33f6df623abcbe
Alec Nikolas Reiter <alecreiter@gmail.com>
2015-09-09 14:01:33 -0400

    Add Fred. Fred is a bastard.

--- tests/fixtures/user.py
+++ tests/fixtures/user.py
@@ -1,6 +1,6 @@
 import pytest
-
 from flaskbb.user.models import User, Guest
+
 
 @pytest.fixture
 def guest():
@@ -12,7 +12,7 @@ def guest():
 def user(default_groups):
     """Creates a user with normal permissions."""
     user = User(username="test_normal", email="test_normal@example.org",
-                password="test", primary_group_id=default_groups[3].id)
+                password="test", primary_group=default_groups[3])
     user.save()
     return user
 
@@ -22,7 +22,7 @@ def moderator_user(user, forum, default_groups):
     """Creates a test user with moderator permissions."""
 
     user = User(username="test_mod", email="test_mod@example.org",
-                password="test", primary_group_id=default_groups[2].id)
+                password="test", primary_group=default_groups[2])
     user.save()
 
     forum.moderators.append(user)
@@ -34,7 +34,7 @@ def moderator_user(user, forum, default_groups):
 def admin_user(default_groups):
     """Creates a admin user."""
     user = User(username="test_admin", email="test_admin@example.org",
-                password="test", primary_group_id=default_groups[0].id)
+                password="test", primary_group=default_groups[0])
     user.save()
     return user
 
@@ -43,6 +43,20 @@ def admin_user(default_groups):
 def super_moderator_user(default_groups):
     """Creates a super moderator user."""
     user = User(username="test_super_mod", email="test_super@example.org",
-                password="test", primary_group_id=default_groups[1].id)
+                password="test", primary_group=default_groups[1])
     user.save()
     return user
+
+
+@pytest.fixture
+def Fred(default_groups):
+    """Fred is an interloper and bad intentioned user, he attempts to
+    access areas he shouldn't, he posts trollish and spammy content,
+    he does everything he can to destroy the board.
+
+    Our job is stop Fred.
+    """
+    fred = User(username='Fred', email='fred@fred.fred',
+                password='fred', primary_group=default_groups[3])
+    fred.save()
+    return fred
