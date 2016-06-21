5837df594f9c18ffe62e90dd4d6ba30fdde98dde
sh4nks <peter.justin@outlook.com>
2016-06-21 14:35:26 +0200

    Use the naive datetime format for MySQL as well
    See the SQLAlchemy docs for more information:
    http://docs.sqlalchemy.org/en/latest/dialects/mysql.html#sqlalchemy.dial
    ects.mysql.DATETIME

--- flaskbb/utils/database.py
+++ flaskbb/utils/database.py
@@ -36,8 +36,8 @@ class UTCDateTime(db.TypeDecorator):
     def process_bind_param(self, value, dialect):
         """Way into the database."""
         if value is not None:
-            # store naive datetime for sqlite
-            if dialect.name == "sqlite":
+            # store naive datetime for sqlite and mysql
+            if dialect.name in ("sqlite", "mysql"):
                 return value.replace(tzinfo=None)
 
             return value.astimezone(pytz.UTC)
@@ -45,7 +45,7 @@ class UTCDateTime(db.TypeDecorator):
     def process_result_value(self, value, dialect):
         """Way out of the database."""
         # convert naive datetime to non naive datetime
-        if dialect.name == "sqlite" and value is not None:
+        if dialect.name in ("sqlite", "mysql") and value is not None:
             return value.replace(tzinfo=pytz.UTC)
 
         # other dialects are already non-naive
