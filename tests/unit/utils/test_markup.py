65edcb6353e6b8487a978b14b195323ab5f0ff0f
Sam Heybey <sam@heybey.org>
2017-10-05 11:17:59 -0400

    add test for invalid lexer

--- tests/unit/utils/test_markup.py
+++ tests/unit/utils/test_markup.py
@@ -25,3 +25,14 @@ print("Hello World")
 
     assert "<pre>" in markdown.render(b_plain)
     assert "highlight" in markdown.render(b_plain_lang)
+
+    # typo in language
+    bad_language = """
+```notpython
+print("Hello World")
+```
+"""
+
+    bad_language_render = markdown.render(bad_language)
+    assert "<pre>" in bad_language_render
+    assert "highlight" not in bad_language_render
