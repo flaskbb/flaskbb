b2b30ab912cecff97382e8fe60fed35d303ed9b5
Peter Justin <peter.justin@outlook.com>
2018-01-06 18:02:59 +0100

    Try instance relative path for wsgi.py first

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ from flaskbb.utils.helpers import ReverseProxyPathFix
 _basepath = os.path.dirname(os.path.abspath(__file__))
 
 # will throw an error if the config doesn't exist
-flaskbb = create_app(config=os.path.join(_basepath, 'flaskbb.cfg'))
+flaskbb = create_app(config='flaskbb.cfg')
 
 #  Uncomment to use the middleware
 #flaskbb.wsgi_app = ReverseProxyPathFix(flaskbb.wsgi_app)
