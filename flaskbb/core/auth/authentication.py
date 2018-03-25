8f1db85790289571ee5d20291894fe82f2f9977e
Alec Nikolas Reiter <alecreiter@gmail.com>
2018-03-25 14:00:48 -0400

    Interface for authentication providers

---
+++ flaskbb/core/auth/authentication.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskbb.core.auth.authentication
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    :copyright: (c) 2014-2018 the FlaskBB Team
+    :license: BSD, see LICENSE for more details
+"""
+
+from abc import abstractmethod
+
+from ..._compat import ABC
+from ..exceptions import BaseFlaskBBError
+
+
+class StopAuthentication(BaseFlaskBBError):
+    """
+    Used by Authentication providers to halt any further
+    attempts to authenticate a user.
+    """
+
+    def __init__(self, reason):
+        super(StopAuthentication, self).__init__(reason)
+        self.reason = reason
+
+
+class AuthenticationProvider(ABC):
+    @abstractmethod
+    def authenticate(self, identifier, secret):
+        pass
+
+    def __call__(self, identifier, secret):
+        return self.authenticate(identifier, secret)
