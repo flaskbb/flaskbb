d98f806de4a99cc07ca4bdc32e2afc4b9af8cfde
Peter Justin <peter.justin@outlook.com>
2018-05-16 19:02:55 +0200

    Bump version: 2.0.0.dev0 → 2.0.0

--- flaskbb/__init__.py
+++ flaskbb/__init__.py
@@ -9,7 +9,7 @@
     :copyright: (c) 2014 by the FlaskBB Team.
     :license: BSD, see LICENSE for more details.
 """
-__version__ = "2.0.0.dev0"
+__version__ = "2.0.0"
 
 import logging
 logger = logging.getLogger(__name__)
