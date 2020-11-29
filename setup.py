f7593c3ef8ac4178c5b3a727736de5e459d63392
Thomas Grainger <tagrain@gmail.com>
2020-11-29 12:41:15 +0000

    remove use of deprecated setup.py test

--- setup.py
+++ setup.py
@@ -2,29 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
-
-
-class PyTestCommand(TestCommand):
-    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest  # noqa
-
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 
 def read(*parts):
@@ -97,9 +76,10 @@ install_requires = [
 ]
 
 
-extras_require = {"postgres": ["psycopg2-binary"]}
-
-tests_require = ["py", "pytest", "pytest-cov", "cov-core", "coverage"]
+extras_require = {
+    "postgres": ["psycopg2-binary"],
+    "test": ["py", "pytest", "pytest-cov", "cov-core", "coverage"],
+}
 
 setup(
     name="FlaskBB",
@@ -127,8 +107,6 @@ setup(
     python_requires=">=3.5",
     install_requires=install_requires,
     extras_require=extras_require,
-    tests_require=tests_require,
-    test_suite="tests",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
@@ -143,5 +121,4 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    cmdclass={"test": PyTestCommand},
 )
