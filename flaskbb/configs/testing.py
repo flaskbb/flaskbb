f63f20cb9ff6180386c0ac8727890e9eaf793eaf
Peter Justin <peter.justin@outlook.com>
2020-12-05 20:14:06 +0100

    Change logger to log to console in testing config

--- flaskbb/configs/testing.py
+++ flaskbb/configs/testing.py
@@ -32,3 +32,36 @@ class TestingConfig(DefaultConfig):
     CELERY_RESULT_BACKEND = "cache"
     CELERY_CACHE_BACKEND = "memory"
     CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+    LOG_DEFAULT_CONF = {
+        'version': 1,
+        'disable_existing_loggers': False,
+
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s %(levelname)-7s %(name)-25s %(message)s'
+            },
+        },
+
+        'handlers': {
+            'console': {
+                'level': 'NOTSET',
+                'formatter': 'standard',
+                'class': 'logging.StreamHandler',
+            },
+        },
+
+        # TESTING: Log to console only
+        'loggers': {
+            'flask.app': {
+                'handlers': ['console'],
+                'level': 'INFO',
+                'propagate': False
+            },
+            'flaskbb': {
+                'handlers': ['console'],
+                'level': 'WARNING',
+                'propagate': False
+            },
+        }
+    }
