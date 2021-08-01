9b4a6478d63aa02622b8acc5c033b2ac34c03207
Peter Justin <peter.justin@outlook.com>
2021-08-01 20:02:22 +0200

    Initialize bs5 tooltips

--- flaskbb/themes/aurora/src/app.js
+++ flaskbb/themes/aurora/src/app.js
@@ -1,13 +1,18 @@
-import "bootstrap-sass/assets/javascripts/bootstrap.min";
+import { Alert, Tooltip, Dropdown, Modal } from 'bootstrap';
 
 import "./app/emoji.js";
 import "./app/editor.js";
 import "./app/flaskbb.js";
-//import "./app/confirm_modal.js";
+import "./app/confirm_modal.js";
 
 
 import "./scss/styles.scss";
 export { BulkActions, show_management_search } from "./app/flaskbb.js";
+
+var tooltips = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltips.map(function (el) {
+  return new Tooltip(el)
+})
 
 // import all assets in ./assets
 function importAll(r) {
