7f68416bdb7629e51feaa54e427e7b22e3837ab1
Peter Justin <peter.justin@outlook.com>
2021-08-05 21:21:12 +0200

    Migrate management templates to bootstrap 5

--- flaskbb/themes/aurora/src/app/confirm_modal.js
+++ flaskbb/themes/aurora/src/app/confirm_modal.js
@@ -2,11 +2,18 @@ import { Modal } from "bootstrap";
 
 var confirmModalElement = document.getElementById("confirmModal");
 if (confirmModalElement) {
-    // PS: Don't forget to use "type=button" for buttons:
-    // https://stackoverflow.com/a/3315016
-    // otherwise you gotta hijack the click event and add a preventDefault()
+    // Usage:
+    // <button type="button" class="btn btn-icon" data-bs-toggle="modal" data-bs-target="#confirmModal">
+    //     <span class="far fa-trash-alt text-danger" data-bs-toggle="tooltip" title="{% trans %}Delete{% endtrans %}"></span>
+    // </button>
+    // PS: Don't forget to use "type=button" for buttons - otherwise you'll submit the form before the modal pops up
+    //     or you gotta hijack the click event and add a preventDefault() to the form
 
     confirmModalElement.addEventListener("show.bs.modal", function(event) {
+        if(event.relatedTarget == undefined || event.relatedTarget.dataset.bsTarget !== "#confirmModal") {
+            return
+        }
+
         // Get the instance of this modal
         let confirmModal = Modal.getInstance(confirmModalElement);
 
